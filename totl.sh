a5f48cd0644df48cf59cf66bcb6b984dda864ce1
aristotlemanolakos <ari@stotle.me>
2016-11-26 12:35:31 -0600

    Added echo statements after important commands conclude. Changed main CSS file from stylesheet.css to style.css. Added paragraph tag to basic HTML template and revised header tag to be more header-y

--- totl.sh
+++ totl.sh
@@ -5,6 +5,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
 NC='\033[0m' # NO COLOR
 
 
@@ -15,8 +16,12 @@ echo -e "${GREEN}You entered:${NC} $text"
 # MAKE PROJECT FOLDER
 mkdir $text && cd $text
 
+echo "New directory created..."
+
 # NEW MAIN HTML FILE
 touch index.html
+
+echo "New HTML file created..."
 
 # SEND TEMPLATE TO HTML FILE
 cat > index.html <<- EOM
@@ -26,25 +31,30 @@ cat > index.html <<- EOM
     <title>$text</title>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="style/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
     <script src="js/app.js"></script>
   </head>
   <body>
-  	<h1>Welcome to: $text</h1>
+  	<h1>Welcome to $text</h1>
+    <p>This is your new project template</p>
   </body>
 </html>
 EOM
 
+echo "HTML template populated..."
+
 # NEW STYLESHEET FILE
 mkdir style && cd style
-touch stylesheet.css
+touch style.css
 cd ..
+echo "New stylesheet created..."
 
 # NEW JAVASCRIPT FILE
 mkdir js && cd js
 touch app.js
 cd ..
+echo "New JavaScript app created..."
 
 # echo "Project $text created, enjoy."
-echo -e "${BLUE}New project template $text created${NC}"
+echo -e "${YELLOW}New project template $text created${NC}"
 
